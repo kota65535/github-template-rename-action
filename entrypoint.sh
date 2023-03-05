e9178d7f95a907dbed42403e55799c457e3fc1ee
Tomohiko Ozawa <kota65535@gmail.com>
2023-03-05 15:46:06 +0900

    d

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,18 +4,19 @@ set -x
 
 export LC_ALL=C
 
+# Configure git
 git config --global user.email "github-actions[bot]@users.noreply.github.com"
 git config --global user.name "github-actions[bot]"
 git config --global --add safe.directory /github/workspace
 
-if ! [[ -v GITHUB_TOKEN ]]; then
-  echo "GITHUB_TOKEN env var is not defined."
+if [[ -n "${GITHUB_TOKEN}" ]]; then
+  echo "GITHUB_TOKEN env var is empty."
   exit 1
 fi
 
 REPO_NAME_FULL="$(gh repo view --json nameWithOwner --jq ".nameWithOwner")"
 
-if ! [[ -v FROM_NAME ]]; then
+if [[ -n "${FROM_NAME}" ]]; then
   FROM_NAME=$(gh api "repos/${REPO_NAME_FULL}" --jq .template_repository.name)
   if [[ -z ${FROM_NAME} ]]; then
     echo "Could not get '${REPO_NAME_FULL}' template repository"
@@ -24,7 +25,7 @@ if ! [[ -v FROM_NAME ]]; then
   fi
 fi
 
-if ! [[ -v TO_NAME ]]; then
+if [[ -n "${TO_NAME}" ]]; then
   TO_NAME="$(gh repo view --json name --jq ".name")"
 fi
 
