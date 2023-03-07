92c1a33d6cc27c0f90c1999a1b9dfabcb5245f11
Tomohiko Ozawa <kota65535@gmail.com>
2023-03-07 13:59:54 +0900

    diff-index

--- src/git.js
+++ src/git.js
@@ -36,7 +36,7 @@ function setGitCredentials(token) {
 function commitAndPush(message) {
   setUserAsBot();
   try {
-    exec("git", ["diff-index", "--quiet", "HEAD"]);
+    exec("git", ["diff-index", "--cached", "--quiet", "HEAD"]);
     return;
   } catch (e) {
     // do nothing
